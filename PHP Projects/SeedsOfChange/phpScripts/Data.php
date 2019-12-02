7deeb166199e0df851e158d04e30dddb596f9b70
lhaufle <ihaufle123@gmail.com>
2019-12-01 20:06:12 -0600

    Create Data.php
    Data class is used to create a object for connecting to the database

---
+++ PHP Projects/SeedsOfChange/phpScripts/Data.php
@@ -0,0 +1,20 @@
+<?php
+class Data {
+//variables to hold database credentials to connect
+private $host = 'localhost';
+private $user = 'lhaufle';
+private $password = 'batman';
+private $db = 'SeedsOfChange';
+//connect to the database
+public function connect(){
+    try{
+        //attempt to connect
+        $dbc = mysqli_connect($this->host, $this->user,$this->password, $this->db);
+        return $dbc;
+    } catch(mysqli_sql_exception $e){
+        //display message if failed
+        echo $e->getMessage();
+        exit;
+    }
+}
+}
