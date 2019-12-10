f734fdef48dab6f4cabd4d1e650fa0a09a410593
lhaufle <ihaufle123@gmail.com>
2019-12-09 21:22:17 -0600

    added front side handling along with ajax

--- PHP Projects/SeedsOfChange/jsScripts/profile.js
+++ PHP Projects/SeedsOfChange/jsScripts/profile.js
@@ -1,16 +1,15 @@
-//not complete,validation to still be added
-
 
 //verify that the dom has loaded
 window.addEventListener('load', () =>{
   
   //listen for a submit from general user info
-  document.getElementById('user_info').addEventListener('click', function(){
-    
+  document.getElementById('user_info').addEventListener('click', function(e){
+    //don't let the browser submit
+    e.preventDefault();
     //get user input values
-    let firstName = document.getElementById('first_name').value;
-    let lastName = document.getElementById('last_name').value;
-    let email = document.getElementById('email').value;
+    let firstName = document.getElementById('first_name');
+    let lastName = document.getElementById('last_name');
+    let email = document.getElementById('email');
     //create xhr object
     let xhr = new XMLHttpRequest();
     //create request type
@@ -18,17 +17,22 @@ window.addEventListener('load', () =>{
     //set headers for the post request
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     //send the request
-    xhr.send("fname="+firstName+"&lname="+lastName+"&email="+email);
+    xhr.send("fname="+firstName.value+"&lname="+lastName.value+"&email="+email.value);
+    //show confirmation
+    firstName.classList.add('is-valid');
+    lastName.classList.add('is-valid');
+    email.classList.add('is-valid');
     
   })
 
   //async request for password
-  document.getElementById('passwordInfo').addEventListener('click', function(){
+  document.getElementById('passwordInfo').addEventListener('click', function(e){
     //get user input
-    let password = document.getElementById('password').value;
-    let repass = document.getElementById('repass').value;
-    //verify the passwords matched
-    
+    let password = document.getElementById('password');
+    let repass = document.getElementById('repass');
+    //verify the passwords match
+    if(password.value == repass.value){
+      e.preventDefault();
       //create object
       let xhr = new XMLHttpRequest();
       //create request
@@ -36,11 +40,17 @@ window.addEventListener('load', () =>{
       //set headers
       xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
       //send request
-      xhr.send("password="+password);
+      xhr.send("password="+password.value);
+      //show confirmation
+      password.classList.add('is-valid');
+      repass.classList.add('is-valid');
       
-    
-      
+    } else {
+      e.preventDefault();
+      //display error if the password does not match
+      document.getElementById('alert').classList.toggle('hide');
+    }
+         
 })
   
- 
 })
